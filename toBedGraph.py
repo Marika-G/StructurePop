6a22ae33fc43f6fb8645795454080a661d39a9a0
Marika-G <marika_groleau@hotmail.com>
2018-07-03 15:34:03 -0400

    Modified to work on combined methylation data

--- toBedGraph.py
+++ toBedGraph.py
@@ -3,45 +3,59 @@ from datetime import datetime
 import pandas as pd
 import sys
 
-
 '''
 	:param 1: Path to CpG genomic coordinate for the methylation data
-	:param 2: Path to pickled methylation data
+	:param 2: Path to pickled combiend methylation data
 	:param 3: Sample name to extract
-	:param 4: Tissue
-	:param 5: Path to save bedGraph
+	:param 4: Header? [Y/N]
+	:param 5: Path to save bedGraphs
 '''
 
 
 def main():
-	## Import data
-	print("Importing data...\t" + str(datetime.now()))
-	coord = pd.read_csv(coordPath, header=0, index_col=0)
-	meth = pd.read_pickle(dataPath)
-	print("Genomic coordinates:\n%s"%(coord.head()))
-	print("Methylation data:\n%s"%(meth.head()))
+    ## Import data
+    print("Importing data...\t" + str(datetime.now()))
+    coord = pd.read_csv(coordPath, header=0, index_col=0)
+    meth = pd.read_pickle(dataPath)
+    print("Genomic coordinates:\n%s" % (coord.head()))
+    print("Methylation data:\n%s" % (meth.head()))
 
-	##Duplicate position column (range is 1)
-	coord['pos2'] = coord['pos']
+    ##Duplicate position column (range is 1)
+    coord['pos2'] = coord['pos'] + 1
 
-	##Join methylation data
-	print("Joining data...\t" + str(datetime.now()))
-	bedInfo = coord.join(meth[sample], how='inner')
+    ##Join methylation data
+    print("Joining data...\t" + str(datetime.now()))
+    bedInfo = coord.join(meth[sample], how='inner')
 
-	##Saving bedGraph
-	print("Saving bedGraph...\t" + str(datetime.now()))
-	# Flags for the browser
-	header = ["track type=bedGraph name=\"%s:%s\" visibility=full"%(tissue, sample), "", "", ""]
-	bedInfo.to_csv("%s%s_%s.bedgraph"%(savePath, sample, dataPath.split('/')[-1].split('.')[0]), sep='\t', header=header, index=False, quoting=csv.QUOTE_NONE)
+    ##Split by tissue
+    placenta = bedInfo.drop("Cord_blood", axis=1)
+    cord = bedInfo.drop("Placenta", axis=1)
 
-	print("End\t" + str(datetime.now()))
+    ##Saving bedGraphs
+    print("Saving bedGraphs...\t" + str(datetime.now()))
+
+    if withHeader == "Y":
+        # Flags for the browser
+        headerP = ["track type=bedGraph name=\"Placenta:%s\" visibility=full" % (sample), "", "", ""]
+        placenta.to_csv("%s%s_Placenta_%s.bedgraph" % (savePath, sample, dataPath.split('/')[-1].split('.')[0]), sep='\t',
+                       header=headerP, index=False, quoting=csv.QUOTE_NONE)
+        headerCB = ["track type=bedGraph name=\"Cord_Blood:%s\" visibility=full" % (sample), "", "", ""]
+        cord.to_csv("%s%s_Cord_Blood_%s.bedgraph" % (savePath, sample, dataPath.split('/')[-1].split('.')[0]),
+                        sep='\t', header=headerCB, index=False, quoting=csv.QUOTE_NONE)
+    else:
+        placenta.to_csv("%s%s_Placenta_%s.bedgraph" % (savePath, sample, dataPath.split('/')[-1].split('.')[0]),
+                        sep='\t', index=False, quoting=csv.QUOTE_NONE)
+        cord.to_csv("%s%s_Cord_Blood_%s.bedgraph" % (savePath, sample, dataPath.split('/')[-1].split('.')[0]),
+                    sep='\t', index=False, quoting=csv.QUOTE_NONE)
+
+    print("End\t" + str(datetime.now()))
 
 
 if __name__ == '__main__':
-	coordPath = sys.argv[1]
-	dataPath = sys.argv[2]
-	sample = sys.argv[3]
-	tissue = sys.argv[4]
-	savePath = sys.argv[5]
+    coordPath = sys.argv[1]
+    dataPath = sys.argv[2]
+    sample = sys.argv[3]
+    withHeader = sys.argv[4]
+    savePath = sys.argv[5]
 
-	main()
+    main()
